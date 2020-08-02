5cecf4ea75c2bf2af2b6c3da82a41524e6e2325a
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-08-02 10:02:19 -0300

    chore Nivel 5 dashboard

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,8 +1,14 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
-  user: object;
+  user: User;
   token: string;
 }
 
@@ -12,7 +18,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -24,21 +30,20 @@ export const AuthProvider: React.FC = ({ children }) => {
     const token = localStorage.getItem('@goBarber:token');
     const user = localStorage.getItem('@goBarber:user');
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
     return {} as AuthState;
   });
   const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post<{ token: string; user: object }>(
-      'sessions',
-      {
-        email,
-        password,
-      },
-    );
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
     const { token, user } = response.data;
     localStorage.setItem('@goBarber:token', token);
     localStorage.setItem('@goBarber:user', JSON.stringify(user));
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
   const signOut = useCallback(() => {
